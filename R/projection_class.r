43f7a408b900974cb6ca4ec5b37423d5735d5347
Russ Jones <RussJones007@gmail.com>
2026-06-28 22:33:47 -0500

    Code execution debugging.  Removed browser() statement

--- R/projection_class.r
+++ R/projection_class.r
@@ -997,6 +997,8 @@ as.data.frame.poparray_projection <- function(x,
                                               include_model = FALSE,
                                               include_confidence = FALSE,
                                               bytes_threshold = 40e6) {
+  
+  browser()
   out <- projection_to_df(
     x,
     include_level = include_level,
